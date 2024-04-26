89f5669785f86f6a4529e511711f1ad491dd25bc
lrivera <lrivera@bancolombia.com.co>
2024-04-26 15:57:08 -0500

    rutas23

--- roop/Frontend/app.py
+++ roop/Frontend/app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 import os
 import base64
+import secrets
+secret_key = secrets.token_hex(16)
 
 app = Flask(__name__)
+app.secret_key = secret_key
+
 captureCounter = 0
 
 @app.route('/')
@@ -34,16 +38,17 @@ def upload():
     img_path = os.path.join('roop/images/', img_name)
     with open(img_path, 'wb') as img_file:
         img_file.write(img_bytes)
-
-    # Ejecutar el comando para procesar la imagen
-    if character_name == 'Unknown':
-        return redirect(url_for('index'))
-    
-    # Procesar la imagen con el comando específico
-    command = f"%cd '/content/roop' && python run.py -s '{character_name}.png' -t 'brad org.mp4' -o 'face_restored_video3.mp4' --keep-frames --keep-fps --temp-frame-quality 1 --output-video-quality 1 --execution-provider cuda --frame-processor face_swapper face_enhancer"
-    os.system(command)
-
+    flash(f"Imagen guardada correctamente", "success")
     return redirect(url_for('index'))
+
+
+@app.route('/transform', methods=['POST'])
+def transform():
+    command = request.json.get('command')
+    if command:
+        os.system(command)
+        return jsonify({'message': 'Transformación completada'})
+    return jsonify({'message': 'Error: comando de transformación no proporcionado'}), 400
 
 
 if __name__ == '__main__':
